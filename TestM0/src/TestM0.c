0327079275cbe8ea86a5e04f9f1994308f67b5d8
Pedro Martos <pimartos@gmail.com>
2017-07-10 20:15:58 -0300

    Initial Release, minor changes from lpc_board_nxp_lpcxpresso_4337 for led control

---
+++ TestM0/src/TestM0.c
@@ -0,0 +1,61 @@
+/*
+===============================================================================
+ Name        : TestM0.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+// TODO: insert other include files here
+
+// TODO: insert other definitions and declarations here
+
+#if defined (M0_SLAVE_PAUSE_AT_MAIN)
+volatile unsigned int pause_at_main;
+#endif
+
+int main(void) {
+
+#if defined (M0_SLAVE_PAUSE_AT_MAIN)
+    // Pause execution until debugger attaches and modifies variable
+    while (pause_at_main == 0) {}
+#endif
+
+#if defined (__USE_LPCOPEN)
+    // Read clock settings and update SystemCoreClock variable
+    SystemCoreClockUpdate();
+#if !defined(NO_BOARD_LIB)
+#if defined (__MULTICORE_MASTER) || defined (__MULTICORE_NONE)
+    // Set up and initialize all required blocks and
+    // functions related to the board hardware
+    Board_Init();
+#endif
+    // Set the LED to the state of "On"
+    Board_LED_Set(1, true);
+    DEBUGSTR("Led Out...\r\n");
+
+#endif
+#endif
+
+    // TODO: insert code here
+
+    // Force the counter to be placed into memory
+    volatile static int i = 0 ;
+    // Enter an infinite loop, just incrementing a counter
+    while(1) {
+        i++ ;
+    }
+    return 0 ;
+}
---
+++ TestM4/src/TestM4.c
@@ -0,0 +1,67 @@
+/*
+===============================================================================
+ Name        : TestM4.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+#if defined (__MULTICORE_MASTER_SLAVE_M0APP) | defined (__MULTICORE_MASTER_SLAVE_M0SUB)
+#include "cr_start_m0.h"
+#endif
+
+// TODO: insert other include files here
+
+// TODO: insert other definitions and declarations here
+
+int main(void) {
+
+#if defined (__USE_LPCOPEN)
+    // Read clock settings and update SystemCoreClock variable
+    SystemCoreClockUpdate();
+#if !defined(NO_BOARD_LIB)
+#if defined (__MULTICORE_MASTER) || defined (__MULTICORE_NONE)
+    // Set up and initialize all required blocks and
+    // functions related to the board hardware
+    Board_Init();
+#endif
+    // Set the LED to the state of "On"
+    Board_LED_Set(0, true);
+    Board_LED_Set(2, true);
+    DEBUGSTR("Two Leds Out...\r\n");
+
+#endif
+#endif
+
+    // Start M0APP slave processor
+#if defined (__MULTICORE_MASTER_SLAVE_M0APP)
+    cr_start_m0(SLAVE_M0APP,&__core_m0app_START__);
+#endif
+
+    // Start M0SUB slave processor
+#if defined (__MULTICORE_MASTER_SLAVE_M0SUB)
+    cr_start_m0(SLAVE_M0SUB,&__core_m0sub_START__);
+#endif
+
+    // TODO: insert code here
+
+    // Force the counter to be placed into memory
+    volatile static int i = 0 ;
+    // Enter an infinite loop, just incrementing a counter
+    while(1) {
+        i++ ;
+    }
+    return 0 ;
+}
